7fddcc4ddd88c86050eea2e178ff205a861c478a
Danny <saifogiodanny@yahoo.com.tw>
2018-07-02 16:43:06 +0800

    update 3

--- auto.sh
+++ auto.sh
@@ -12,7 +12,7 @@ do
       do
        /mingw64/bin/git add $f
        /mingw64/bin/git commit -m "$(date +%H-%M-%S)"
-       /mingw64/bin/git remote add https://github.com/dioga51111/run_shell_test.git
+       /mingw64/bin/git remote add origin https://github.com/dioga51111/run_shell_test.git
        /mingw64/bin/git push -u origin master
        /mingw64/bin/git remote remove orgin
       done
